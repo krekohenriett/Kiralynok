538f21a9a89ecf3921849a641f8ef0ada95be355
krekohenriett <heni.kreko@gmail.com>
2021-02-19 10:10:31 +0100

    4.feladat kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 public class Kiralynok {
     public static void main(String[] args) throws IOException {
-        
+         //4.feladat
+        System.out.println("4.feladat: üres tábla megjelenítése:");
+        Tabla tabla = new Tabla('#');
+        tabla.megjelenit();
     }
     
 }
